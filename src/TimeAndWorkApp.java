4c6b6588acc15f83fbcd1fa6a0f30d2ff7b83e36
Balaji Babu Chinthaginjala <balajibabuchinthaginjala@gmail.com>
2023-03-03 17:11:01 +0530

    Time and work main

--- src/TimeAndWorkApp.java
+++ src/TimeAndWorkApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TimeAndWorkApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       System.out.println(Leaving.leaving(12,18,18,5));
+       //System.out.println(Leaving.leaving(12,18,18,5));
         System.out.println("1. If A and B can do a piece of work in X and Y days respectively while working\n" +
                 "alone, they will together take time to complete it.");
         System.out.println("2. If A , B , C can do a piece of work in X , Y , Z days respectively while\n" +
@@ -20,19 +20,22 @@ public class TimeAndWorkApp {
                  the work be completed?""");
         System.out.println("7.A is K times as good a worker as B and takes X days less than B to finish the\n" +
                            " work. Then the amount of time required by A and B working together is");
-        System.out.println("8.If A can finish work in X days and B in Y days and A, B and C together in S days then c alone b_c and a_b alone\n");
+        System.out.println("8.If A can finish work in X days and B in Y days and A, B and C together in S days then" +
+                " c alone b_c and a_b alone can do in:\n");
+        System.out.println("9.A can complete  X piece of work in 'n1' days and\n" +
+                "    B-> Y piece of work in 'n2' days in how many days both A and B can complete");
         System.out.println();
         System.out.println("Enter your Choice");
         int choice = sc.nextInt();
         switch (choice) {
-            case 1:
+            case 1 -> {
                 System.out.println("Enter work Done By A");
                 double x = sc.nextDouble();
                 System.out.println("Enter Work done by B");
                 double y = sc.nextDouble();
                 System.out.println(WorkingAlone_TogetherToComplete.A_BTogether(x, y));
-                break;
-            case 2:
+            }
+            case 2 -> {
                 System.out.println("Enter work Done By A");
                 double x1 = sc.nextDouble();
                 System.out.println("Enter Work done by B");
@@ -40,8 +43,8 @@ public class TimeAndWorkApp {
                 System.out.println("Enter work Done by C");
                 double z1 = sc.nextDouble();
                 System.out.println(WorkingAlone_TogetherToComplete.A_B_CTogether(x1, y1, z1));
-                break;
-            case 3:
+            }
+            case 3 -> {
                 System.out.println("Enter the work done by A and B");
                 double A_BWork = sc.nextDouble();
                 System.out.println("Enter the work done by B and C");
@@ -61,9 +64,9 @@ public class TimeAndWorkApp {
                     case 4 -> System.out.println(WorkingTogether_AloneToComplete.c_alone(A_BWork, B_CWork, A_CWork));
                     default -> System.out.println("Enter valid input");
                 }
-            case 4:
-                ChainRule.main(args);
-            case 5:
+            }
+            case 4 -> ChainRule.main(args);
+            case 5 -> {
                 System.out.println("enter value for no of mans");
                 double m1 = sc.nextDouble();
                 System.out.println("Enter value for no of women/boys");
@@ -77,8 +80,8 @@ public class TimeAndWorkApp {
                 double result1 = ChainRule.type1(m1, w1, d1, m2, w2);
                 System.out.println("The total no of days to complete the work by " + m2 + " men and " + w2 + " women is: "
                         + result1 + " days");
-                break;
-            case 6:
+            }
+            case 6 -> {
                 System.out.println("Enter A work");
                 double x2 = sc.nextDouble();
                 System.out.println("Enter B work");
@@ -92,55 +95,63 @@ public class TimeAndWorkApp {
                 } else {
                     System.out.println(Leaving.leaving_B(x2, y2, z2));
                 }
-                break;
-            case 7:
+            }
+            case 7 -> {
                 System.out.println("Enter the A is how times better than B");
                 double k = sc.nextDouble();
                 System.out.println("Enter how many days less than B");
                 double k1 = sc.nextDouble();
                 double result = SingleProblems.ab_together(k, k1);
                 System.out.println("total time taken to complete the work By A and B is: " + result + " days");
-                break;
-            case 8:
+            }
+            case 8 -> {
                 System.out.println("enter A alone work:");
                 double A1 = sc.nextInt();
                 System.out.println("enter B alone work:");
                 double B1 = sc.nextInt();
                 System.out.println("enter A+B+C work:");
                 double C1 = sc.nextInt();
-
                 System.out.println("1.C alone work");
                 System.out.println("2.B_C work");
                 System.out.println("3.A_C work");
                 System.out.println("4.A_B work");
                 int option = sc.nextInt();
                 switch (option) {
-                    case 1:
+                    case 1 -> {
                         double c_work = WorkingAlone_TogetherToComplete.c_work(A1, B1, C1);
                         System.out.println("C alone can do in: " + c_work + " days");
-                        break;
-                    case 2:
+                    }
+                    case 2 -> {
                         double B_C_work = WorkingAlone_TogetherToComplete.B_C_work(A1, B1, C1);
                         System.out.println("B and C complete the work in: " + B_C_work + " days");
-                        break;
-                    case 3:
+                    }
+                    case 3 -> {
                         double A_C_work = WorkingAlone_TogetherToComplete.A_C_work(A1, B1, C1);
                         System.out.println("A and C complete the work in: " + A_C_work + " days");
-                        break;
-
-                    case 4:
+                    }
+                    case 4 -> {
                         double A_B_work = WorkingAlone_TogetherToComplete.A_B_work(A1, B1, C1);
                         System.out.println("A and B complete the work in: " + A_B_work + " days");
-                        break;
+                    }
                 }
-                break;
 
 
+            }
+            case 9->{
+                System.out.println("Enter the value for part of work done by A(Total Work)");
+                double x= sc.nextDouble();
+                System.out.println("Enter how many days taken to complete the work by A");
+                double d1= sc.nextDouble();
+                System.out.println("Enter the value for part of work done by B(Total Work)");
+                double y= sc.nextDouble();
+                System.out.println("Enter how many days taken to complete the work by B");
+                double d2= sc.nextDouble();
+                System.out.println(WorkingAlone_TogetherToComplete.A_BWork(x,d1,y,d2));
+
+            }
 
 
-
-            default:
-                System.out.println("Select correct Option");
+            default -> System.out.println("Select correct Option");
         }
 
 
